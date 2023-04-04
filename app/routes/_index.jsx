ec6ab3d9af39ea4f11a09813dda05651154eefaf
BradBettig <brad.bettig9@gmail.com>
2023-04-04 12:36:32 -0700

    Added cookies to worker and display it

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -3,13 +3,13 @@ import React, { useEffect, useState } from 'react';
 export default function Index() {
   const [latitude, setLatitude] = useState('-');
   const [longitude, setLongitude] = useState('-');
+  const [uniqueUserId, setUniqueUserId] = useState(null);
 
+  //Geolocation
   useEffect(() => {
     const success = (position) => {
       setLatitude(position.coords.latitude);
       setLongitude(position.coords.longitude);
-
-      // Send the location data to your server or process it as needed
     };
 
     const error = (err) => {
@@ -30,6 +30,17 @@ export default function Index() {
     }
   }, []);
 
+  //Cookie
+  useEffect(() => {
+    const getCookie = (name) => {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(';').shift();
+    };
+
+    setUniqueUserId(getCookie('uniqueUserId'));
+  }, []);
+
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Hello</h1>
@@ -37,6 +48,12 @@ export default function Index() {
         Latitude: <span>{latitude}</span>
         <br />
         Longitude: <span>{longitude}</span>
+        <br />
+        {uniqueUserId && (
+        <p>
+          Your unique user ID is: <strong>{uniqueUserId}</strong>
+        </p>
+      )}
       </div>
     </div>
   );
