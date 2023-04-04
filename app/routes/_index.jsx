9d96d745de85c8892ff7c25e1eb49e834e71edf9
BradBettig <brad.bettig9@gmail.com>
2023-04-04 14:25:55 -0700

    trying web3 import

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { web3 } from 'Web3';
+import { Web3 } from 'web3';
 
 export default function Index() {
   const [latitude, setLatitude] = useState('-');
